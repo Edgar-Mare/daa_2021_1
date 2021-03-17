3de9bd8c0e4035f77194f975ff552985d4dab8ec
Edgar <wiiedgar7@gmail.com>
2021-03-17 10:24:37 -0600

    interrupcion

---
+++ interrupciones3.py
@@ -0,0 +1,24 @@
+import time
+import keyboard
+t=0
+bandera=True
+decision=''
+inicio = True
+while inicio:
+    decision=str(input('Iniciar s/n: '))
+    if decision == "s":
+        bandera = True
+        while bandera:
+            #time.sleep(1)
+            print (t)
+            t=t+1
+            if keyboard.is_pressed('p'):
+                print('se presionó parar!')
+                print(f'Valor de contador: {t}')
+                bandera=False
+
+    elif decision == 'n':
+        inicio = False
+    else:
+        time.sleep(1)
+        print('error')
