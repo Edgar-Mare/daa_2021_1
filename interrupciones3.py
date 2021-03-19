a5ed7b97d7a2496aea5376677a14e51370e7c333
Edgar <wiiedgar7@gmail.com>
2021-03-18 23:15:37 -0600

    Version de revicion

--- interrupciones3.py
+++ interrupciones3.py
@@ -8,23 +8,23 @@ inicio = True
 
 
 
-while inicio:
+while inicio: #Punto base del sitema
     decision=str(input('Iniciar s/n: '))
     if decision == "s":
         bandera_keyboard = True
         bandera_contador = True
 
-        while bandera_keyboard and bandera_contador:
+        while bandera_keyboard and bandera_contador:#Requisitos para activar las interrupciones
             
             time.sleep(0.001)
           
             print (count)
             count=count+1
-            if count % 1000 == 0:
+            if count % 1000 == 0: #Interrupción por numero de iteraciones  (interrupción por software)
                 print(f'Se realizaron {count} iteraciones')
                 bandera_contador = False
 
-            if keyboard.is_pressed('p'):
+            if keyboard.is_pressed('p'):#Interrupción por teclado (interrupción por hardware)
                 print('se presionó parar!')
                 print(f'Valor de contador: {count}')
                 bandera_keyboard=False
